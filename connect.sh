b991dfc4c2718b5c54ce8e779e2cb42a42b833bb
Alessandro Vioni <alessandro.vioni@objectway.it>
2017-05-15 21:34:37 +0200

    Commented file for better comprehension.

--- connect.sh
+++ connect.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Change the DNS with the choosen protocol as per README.md
 
 /usr/sbin/networksetup -setdnsservers Wi-Fi 10.4.0.1
 /usr/sbin/networksetup -setsearchdomains Wi-Fi openvpn
