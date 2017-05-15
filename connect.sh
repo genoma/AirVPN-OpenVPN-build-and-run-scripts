ad29922b8362adf02c7db69cc884a036447206d5
Alessandro Vioni <alessandro.vioni@objectway.com>
2017-05-15 22:45:57 +0200

    Slightly modified the comment.

--- connect.sh
+++ connect.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# Change the DNS with the choosen protocol as per README.md
 
+# Change the DNS with the choosen protocol as per README.md
 /usr/sbin/networksetup -setdnsservers Wi-Fi 10.4.0.1
 /usr/sbin/networksetup -setsearchdomains Wi-Fi openvpn
 /sbin/pfctl -Fa -f killswitch.pf.conf -e
