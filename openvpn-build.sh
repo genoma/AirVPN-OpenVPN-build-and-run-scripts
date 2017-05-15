a4d1002bf7b0f6346da3b46858e46c2ec6c86abc
Alessandro Vioni <alessandro.vioni@objectway.it>
2017-05-15 21:31:23 +0200

    Build script for OpenVPN.
    A simple build script for OpenVPN compiled with LibreSSL or (commented) OpenSSL.

---
+++ openvpn-build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd ~/openvpn
+make clean
+autoreconf -i -v -f
+# Libressl
+./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --enable-pkcs11 --with-crypto-library=openssl OPENSSL_LIBS="-L/usr/local/opt/libressl/lib -lssl -lcrypto" OPENSSL_CFLAGS="-I/usr/local/opt/libressl/include"
+
+# Openssl
+# ./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --with-crypto-library=openssl OPENSSL_LIBS="-L/usr/local/opt/openssl/lib -lssl -lcrypto" OPENSSL_CFLAGS="-I/usr/local/opt/openssl/include"
+
+make -j4
+make install
