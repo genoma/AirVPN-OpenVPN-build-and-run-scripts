cd8b646ae7856a3e5acb2773bd9be621f55c37a1
Alessandro Vioni <alessandro.vioni@objectway.com>
2017-05-16 21:44:35 +0200

    Removed unused build option.
    --enable-pkcs11 don’t know what it does and don’t care enough honestly.

--- openvpn-build.sh
+++ openvpn-build.sh
@@ -4,7 +4,7 @@ cd ~/openvpn
 make clean
 autoreconf -i -v -f
 # Libressl
-./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --enable-pkcs11 --with-crypto-library=openssl OPENSSL_LIBS="-L/usr/local/opt/libressl/lib -lssl -lcrypto" OPENSSL_CFLAGS="-I/usr/local/opt/libressl/include"
+./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --with-crypto-library=openssl OPENSSL_LIBS="-L/usr/local/opt/libressl/lib -lssl -lcrypto" OPENSSL_CFLAGS="-I/usr/local/opt/libressl/include"
 
 # Openssl
 # ./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --with-crypto-library=openssl OPENSSL_LIBS="-L/usr/local/opt/openssl/lib -lssl -lcrypto" OPENSSL_CFLAGS="-I/usr/local/opt/openssl/include"
