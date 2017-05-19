bdbc13815eee9a12f2c0543c72ceb38039c9746e
Alessandro Vioni <alessandro.vioni@objectway.com>
2017-05-19 21:22:34 +0200

    Reverted back to the correct UDP DNS.

--- connect.sh
+++ connect.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Change the DNS with the choosen protocol as per README.md
-/usr/sbin/networksetup -setdnsservers Wi-Fi 10.50.0.1
+/usr/sbin/networksetup -setdnsservers Wi-Fi 10.4.0.1
 /usr/sbin/networksetup -setsearchdomains Wi-Fi openvpn
 /sbin/pfctl -Fa -f killswitch.pf.conf -e
 # Clear DNS cache
