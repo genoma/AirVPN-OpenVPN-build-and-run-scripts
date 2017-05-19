11d2cf6d94f8c21d98c5952f664a96cbc430b84b
Alessandro Vioni <alessandro.vioni@objectway.com>
2017-05-19 21:05:53 +0200

    DNS cache should be handled better.

--- connect.sh
+++ connect.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # Change the DNS with the choosen protocol as per README.md
-/usr/sbin/networksetup -setdnsservers Wi-Fi 10.4.0.1
+/usr/sbin/networksetup -setdnsservers Wi-Fi 10.50.0.1
 /usr/sbin/networksetup -setsearchdomains Wi-Fi openvpn
 /sbin/pfctl -Fa -f killswitch.pf.conf -e
+# Clear DNS cache
+sudo /usr/bin/killall -HUP mDNSResponder
+sudo /usr/bin/dscacheutil -flushcache
--- disconnect.sh
+++ disconnect.sh
@@ -3,3 +3,6 @@
 /usr/sbin/networksetup -setdnsservers Wi-Fi empty
 /usr/sbin/networksetup -setsearchdomains Wi-Fi empty
 /sbin/pfctl -Fa -f /etc/pf.conf -d
+# Clear DNS cache
+sudo /usr/bin/killall -HUP mDNSResponder
+sudo /usr/bin/dscacheutil -flushcache
